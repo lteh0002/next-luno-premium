7d712079bdb82bb57581e9999e3c2b22af4d77ea
lteh0002 <tehlinhui@gmail.com>
2023-01-12 15:37:54 +0800

    Add function for retrieving Luno BTC prices in MYR

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 require('dotenv').config()
+
+async function getBTCMYRPrice() {
+    const data = await fetch('https://api.luno.com/api/1/orderbook_top?pair=XBTMYR')
+    const result = await data.json()
+    console.log(result.asks[0].price)
+    // https://www.branch.io/glossary/query-parameters/
+}
+
+getBTCMYRPrice()
