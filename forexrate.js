85caa487325d9c6a6a9093b530add4d8cf3443d8
lteh0002 <tehlinhui@gmail.com>
2023-01-17 17:21:02 +0800

    Added API key error and API key limit error handling in code

--- forexrate.js
+++ forexrate.js
@@ -1,19 +1,26 @@
 export async function getUSDMYRForexRate() {
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", process.env.YOUR_API);
+var myHeaders = new Headers();
+myHeaders.append("apikey", process.env.YOUR_API);
 
-    var requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: myHeaders
-    };
+var requestOptions = {
+method: 'GET',
+redirect: 'follow',
+headers: myHeaders
+};
 
-    let data = await fetch('https://api.apilayer.com/fixer/latest?symbols=MYR&base=USD', requestOptions)
-    if (data.status !== 200) {
-        return 'Failed to retrieve data'
-    } else {
-        let convertData = await data.json()
-        return Number(convertData.rates['MYR'])
-    }
-    
+let data = await fetch('https://api.apilayer.com/fixer/latest?symbols=MYR&base=USD', requestOptions)
+if (data.status !== 200) {
+  if (data.status === 401) {
+    return ('Please check your API Credential')
+  } else if (data.status === 429) {
+      return('Please check your API Daily/Monthly Limit')
+  } else {
+      return('Failed to retrieve data')
+  }
+} else {
+    let convertData = await data.json()
+    return Number(convertData.rates['MYR'])
 }
+}
+
+getUSDMYRForexRate()
